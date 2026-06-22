dc48c9b46cea8cf904c66d10480cfec80896371f
Aresha-Khalid072 <areshakhalid072@gmail.com>
2026-06-22 19:47:32 +0500

    FetchGIF Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
-import { fetchPhotos, fetchVideos } from './api/mediaApi'
+import React from "react";
+import { fetchGIF, fetchPhotos, fetchVideos } from "./api/mediaApi";
 
 const App = () => {
-
-
- 
   return (
-    <div className='h-screen w-full text-white bg-gray-950'>
-      <button  className= 'bg-green-600 px-2 py-3 m-5'  onClick={async()=>{
-     const data= await fetchPhotos('cat')
-     console.log(data.results)
-      }}>Get Photos</button>
+    <div className="h-screen w-full text-white bg-gray-950">
+      <button
+        className="bg-green-600 px-2 py-3 m-5"
+        onClick={async () => {
+          const data = await fetchPhotos("cat");
+          console.log(data.results);
+        }}
+      >
+        Get Photos
+      </button>
 
+      <button
+        className="bg-green-600 px-2 py-3 m-5"
+        onClick={async () => {
+          const data = await fetchVideos("cat");
+          console.log(data.videos);
+        }}
+      >
+        Get Videos
+      </button>
 
-       <button   className= 'bg-green-600 px-2 py-3 m-5'  
-      onClick={async()=>{
-     const data= await fetchVideos('cat')
-     console.log(data.videos)
-      }}>Get Videos</button>
+      <button
+        className="bg-green-600 px-2 py-3 m-5"
+        onClick={async () => {
+          const data = await fetchGIF("cat");
+          console.log(data);
+        }}
+      >
+        Get GIF
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -1,26 +1,34 @@
-import axios from 'axios'
+import axios from "axios";
 
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
 
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
 
-const UNSPLASH_KEY=import.meta.env.VITE_UNSPLASH_KEY
+const KLIPY_KEY = import.meta.env.VITE_KLIPY_KEY;
 
-const PEXELS_KEY=import.meta.env.VITE_PEXELS_KEY
-
-
- export async function fetchPhotos(query,page=1,per_page=20){
- const res=await axios.get('https://api.unsplash.com/search/photos',{
-    params:{query,page,per_page},
-    headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
- })
-//  console.log(res);
-return res.data
+export async function fetchPhotos(query, page = 1, per_page = 20) {
+  const res = await axios.get("https://api.unsplash.com/search/photos", {
+    params: { query, page, per_page },
+    headers: { Authorization: `Client-ID ${UNSPLASH_KEY}` },
+  });
+  //  console.log(res);
+  return res.data;
 }
 
+export async function fetchVideos(query, per_page = 20) {
+  const res = await axios.get("https://api.pexels.com/videos/search", {
+    params: { query, per_page },
+    headers: { Authorization: PEXELS_KEY },
+  });
+  return res.data;
+}
 
- export async function fetchVideos(query,per_page=20){
- const res=await axios.get('https://api.pexels.com/videos/search',{
-    params:{query,per_page},
-    headers:{Authorization:PEXELS_KEY}
- })
-return res.data
+export async function fetchGIF(query, page = 1, per_page = 20) {
+  const res = await axios.get(
+    `https://api.klipy.com/api/v1/${KLIPY_KEY}/gifs/search`,
+    {
+      params: { q: query, page, per_page },
+    },
+  );
+  return res.data;
 }
